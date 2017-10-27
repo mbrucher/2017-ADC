262a9abbfe5637c540238ae6c9881e95bd14b102
Matthieu Brucher <matthieu.brucher@gmail.com>
2017-10-27 17:43:20 +0100

    Starting with an empty project

---
+++ main.cpp
@@ -0,0 +1,9 @@
+/**
+ * \ file main.pp
+ * Runner file for ATK ADC test
+ */
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_MODULE ATKADC
+#include <boost/test/unit_test.hpp>
